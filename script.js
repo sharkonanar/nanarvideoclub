201a85134b479f75f5c7964e141e6938aa527fef
sharkonanar <carrygerard01@gmail.com>
2025-12-29 17:42:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 window.onload = function() {
     const grille = document.getElementById('video-grid');
     
-    if (typeof videos !== 'undefined') {
-        grille.innerHTML = ""; 
-
-        videos.forEach(film => {
-            grille.innerHTML += `
-                <div class="video-card">
-                    <a href="video.html?id=${film.id}">
-                        <img src="${film.image}" alt="${film.titre}">
-                        <div class="video-info">
-                            <h3>${film.titre}</h3>
-                            <p>${film.resume}</p>
-                        </div>
-                    </a>
-                </div>
-            `;
-        });
-    }
+    // On boucle sur tes films (qui sont dans videos.js)
+    videos.forEach(film => {
+        grille.innerHTML += `
+            <div class="video-item">
+                <a href="video.html?id=${film.id}">
+                    <img src="${film.image}" alt="${film.titre}">
+                    <div class="video-info">
+                        <h3>${film.titre}</h3>
+                        <p>${film.resume}</p>
+                    </div>
+                </a>
+            </div>
+        `;
+    });
 };
