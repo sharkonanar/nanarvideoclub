c07fa03e56cd2f0e114440dfc42b323331390d1e
sharkonanar <carrygerard01@gmail.com>
2025-12-29 18:12:52 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -4,7 +4,7 @@ const videos = [
         titre: "Les guerriers du Bronx",
         resume: "Une épopée incroyable avec des motards aux looks SM et des flics pas gentils qu' ils sont méchants.",
         image: "images/guerriersbronx.jpg",
-        videoUrl: "https://www.youtube.com/embed/cCzi0HWhras" 
+        videoUrl: "https://www.youtube.com/embed/cCzi0HWhras",
         note: 4 // <--- Ajoute la note ici (de 1 à 5)
     },
     {
