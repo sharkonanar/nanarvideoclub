827e98b6a47dcffde1e1e726519fb794067e2932
sharkonanar <carrygerard01@gmail.com>
2025-12-30 16:58:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,23 @@ const grid = document.getElementById('video-grid');
 
 function displayVideos() {
     if (!grid || typeof videos === 'undefined') return;
-
     grid.innerHTML = ''; 
 
     videos.forEach(film => {
-        // On crée la boîte de la cassette
         const card = document.createElement('div');
-        
-        // On insère le lien et le design tranche
         card.innerHTML = `
-            <a href="video.html?id=${film.id}" style="text-decoration: none; color: inherit;">
+            <a href="video.html?id=${film.id}" style="text-decoration: none;">
                 <div class="vhs-box">
-                    <img src="${film.image}" alt="${film.titre}" class="vhs-poster">
+                    <img src="images/tranche.jpg" class="vhs-tranche-img">
+                    
+                    <img src="${film.image}" class="vhs-poster">
+                    
                     <span class="vhs-title">${film.titre}</span>
                 </div>
             </a>
         `;
-        
         grid.appendChild(card);
     });
 }
 
-// On lance l'affichage
 displayVideos();
